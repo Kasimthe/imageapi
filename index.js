8c65989411841b9bc864ece3d4ca6cd0e3c5463a
kasimytcreation@gmail.com <YOUR_EMAIL_ADDRESS>
2023-04-30 23:11:48 +0530

    dlete images

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const router = require("./src/routers/image");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Enable CORS
 app.use(cors());
